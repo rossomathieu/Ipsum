125f9fd74b05d22f76af1eac88e4bc079a9c1ae4
ross <rossomathieu@gmail.com>
2014-05-17 01:06:12 -0400

    Started implementation of MD5

--- src/libaeg/libaeanim/AEMD5Animation.hpp
+++ src/libaeg/libaeanim/AEMD5Animation.hpp
@@ -3,3 +3,10 @@
 class AEMD5Animation {
 
 public:
+
+	AEMD5Animation();
+	virtual ~AEMD5Animation();
+	
+	//Load an animation from the animation file
+	bool loadMD5Animation(const std::string& filename);
+	
