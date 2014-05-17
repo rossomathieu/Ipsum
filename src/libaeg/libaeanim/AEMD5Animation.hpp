c7947fdc78a96488205e1399a9977e7928a855a9
ross <rossomathieu@gmail.com>
2014-05-17 13:01:48 -0400

    Bump

--- src/libaeg/libaeanim/AEMD5Animation.hpp
+++ src/libaeg/libaeanim/AEMD5Animation.hpp
@@ -6,7 +6,13 @@ public:
 
 	AEMD5Animation();
 	virtual ~AEMD5Animation();
-	
+
 	//Load an animation from the animation file
 	bool loadMD5Animation(const std::string& filename);
-	
+
+	//Update the animation's joint set.
+	void update(float deltaTime);
+
+	//Draw the animated skeleton
+	void render();
+
